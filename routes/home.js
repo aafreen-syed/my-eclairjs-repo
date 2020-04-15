8746be6bfa28762d07c3c476b4508c5e9229d0ab
aafreen-syed <aafreen.syed@alphonsoglobal.com>
2020-04-15 15:17:45 +0530

    eclair js wordcount app

---
+++ routes/home.js
@@ -0,0 +1,8 @@
+/**
+ 
+ * home.js - Default route for GET requests to home page.
+ */
+ 
+exports.index = function(req, res){
+  res.render("index", {title: "Using EclairJS to Count Words in a File"});
+};
